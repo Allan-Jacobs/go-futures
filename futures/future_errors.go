88f1ecd48b91f5e0222cb522756ae7d7ffddcc63
RedstoneBlocks <ajjacobs01@kellerisd.com>
2022-12-01 23:40:30 -0600

    create an error aggregation type

---
+++ futures/future_errors.go
@@ -0,0 +1,33 @@
+package futures
+
+import "strings"
+
+// An aggregate error type
+type ErrorAggregation interface {
+	error
+	Errors() []error
+}
+
+type errorAggregation []error
+
+func (e errorAggregation) Error() string {
+	if len(e) == 0 {
+		return ""
+	}
+	if len(e) == 1 {
+		return e[0].Error()
+	}
+	msgs := make([]string, len(e))
+	for _, err := range e {
+		msgs = append(msgs, err.Error())
+	}
+	return "[" + strings.Join(msgs, ", ") + "]"
+}
+
+func (e errorAggregation) Errors() []error {
+	return e
+}
+
+func aggregate(errs []error) ErrorAggregation {
+	return errorAggregation(errs)
+}
